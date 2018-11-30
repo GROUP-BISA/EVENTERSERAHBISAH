a1e6d4b0eec2d09bf7332da8d4a29f8caf1a506e
aandroomeedaa <41657603+aandroomeedaa@users.noreply.github.com>
2018-11-30 14:21:13 +0700

    Revert "Development"

--- server/helpers/encrypt-password.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = {
-
-    getEncrypt(password) {
-        return crypto.createHmac('sha256', process.env.SECRET_PWD).update(password).digest('hex');
-    }
-
-};
